2dbae441ab514c0633c7453bff8eb895b9d00948
HenryEBG <berganza386@gmail.com>
2024-04-09 19:37:37 -0400

    Formating the result for 1 week

--- script.js
+++ script.js
@@ -2,22 +2,33 @@
  * Exercise 1
  * Growing Pains
  */
-
+// declaring constants
 const PI = 3.1415
-const radius = 10
-const area = PI*radius*radius
-
 const minSpace = 0.8
-const initPlant = 20
-const initArea = minSpace*initPlant
 
-console.log(`The total area is ${area} square meters`)
-let numWeeks=2
-console.log(`Number of weeks are ${numWeeks}`)
-let plantGrows=numWeeks*2*initArea
-console.log(`The area used is ${plantGrows} square meters`)
+//declaring variables 
+let radius = 5
+let area = PI*radius*radius
+let initPlant = 20
+let initArea = minSpace*initPlant
+let numWeeks
+let plantGrows
+/**
+ * Example with 1 week
+ */
+// number of weeks
+numWeeks=1
+// calculate the area cover with plants
+plantGrows=(numWeeks-1)*2*initArea+initArea//calculate area occuped
+//calculate percent of area cover with plants
 let percentPlanted=plantGrows/area
+
+// Console logs to get the answers
+console.log(`The total area is ${area} square meters`)
+console.log(`Number of weeks are ${numWeeks}`)
+console.log(`The area used is ${plantGrows} square meters`)
 console.log(`The percent used is ${percentPlanted*100}%`)
+console.log(`***************************************************`)
 
 
 if(percentPlanted>0.8){
@@ -29,3 +40,28 @@ if(percentPlanted>0.8){
 }else{
   console.log(`The garden need to be Planted, is under the 50% of the capacity`)
 }
+
+
+/**
+ * Part 2: Thinking Bigger
+ */
+// initial plants
+initPlant=100
+// initial area
+initArea = minSpace*initPlant
+// initial number of weeks
+numWeeks=10
+// variable to save area needed
+let areaNeeded=0
+// variable to save radius of the area needed
+let radiusNeeded=0
+
+areaNeeded=((initArea*numWeeks*2)-area)/0.8
+radiusNeeded=Math.sqrt(areaNeeded/PI)
+console.log(`we need an additional space of a radius of ${radiusNeeded} to not pruned the garden for ${numWeeks} weeks`)
+
+
+/**
+ * Part 3:  Errors in Judgement
+ * 
+ */
